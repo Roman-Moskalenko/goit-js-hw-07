3181cf8b33d103541e808968ef43a8865fccb0ad
Roman-Moskalenko <92758017+Roman-Moskalenko@users.noreply.github.com>
2022-09-25 23:43:30 +0300

    hw7

---
+++ js/01-gallery.js
@@ -0,0 +1,27 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+
+console.log(createImageCardMarkup(galleryItems));
+const galleryContainer = document.querySelector( '.gallery')
+const galleryMarkup = createImageCardMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML( 'beforeend', galleryMarkup)
+function createImageCardMarkup(galleryItems){
+    return galleryItems.map(({preview, original, description }) =>{
+    return
+    `<div class="gallery__item">
+    <a class="gallery__link" href="large-image.jpg">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </div>`
+   }).join(" ")
+   
+}
+
+
+
