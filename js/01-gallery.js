f93cb29bad8eaedb4c2cf5635bd7d2bf7c4d96b7
Roman-Moskalenko <92758017+Roman-Moskalenko@users.noreply.github.com>
2022-09-26 23:25:56 +0300

    hw7 final

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,16 +1,17 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
-
-console.log(createImageCardMarkup(galleryItems));
 const galleryContainer = document.querySelector( '.gallery')
+
 const galleryMarkup = createImageCardMarkup(galleryItems);
+
+
+
 galleryContainer.insertAdjacentHTML( 'beforeend', galleryMarkup)
 function createImageCardMarkup(galleryItems){
-    return galleryItems.map(({preview, original, description }) =>{
-    return
-    `<div class="gallery__item">
-    <a class="gallery__link" href="large-image.jpg">
+    return galleryItems.map(({preview, original, description }) => {
+    return`<div class="gallery__item">
+    <a class="gallery__link" 
+    href="${original.value}">
       <img
         class="gallery__image"
         src="${preview}"
@@ -19,9 +20,25 @@ function createImageCardMarkup(galleryItems){
       />
     </a>
   </div>`
-   }).join(" ")
-   
+   }).join(" ")  
 }
+
+const onGalleryContainerClick = (e) => {
+  e.preventDefault();
+  if (e.target === e.currentTarget) return;
+  const source = e.target.dataset.source;
+  const instance = basicLightbox.create(
+    `<div class="modal">
+      <img src="${source}" 
+      width="900"
+      height="500">
+    </div>`
+  );
+  instance.show();
+  console.log(e.target);
+};
+
+galleryContainer.addEventListener("click", onGalleryContainerClick);
 
 
 
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,41 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryContainer = document.querySelector( '.gallery')
 
-console.log(galleryItems);
+const galleryMarkup = createImageCardMarkup(galleryItems);
+
+
+galleryContainer.insertAdjacentHTML( 'beforeend', galleryMarkup)
+function createImageCardMarkup(galleryItems){
+    return galleryItems.map(({preview, original, description }) => {
+    return`<div class="gallery__item">
+    <a class="gallery__link" 
+    href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </div>`
+   }).join(" ")  
+}
+const lightbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+    captionType: "alt"})
+// const onGalleryContainerClick = (e) => {
+//   e.preventDefault();
+//   if (e.target === e.currentTarget) return;
+//   const source = e.target.dataset.source;
+//   const instance = basicLightbox.create(
+//     `<div class="modal">
+//       <img src="${source}" 
+//       width="1100"
+//       height="700">
+//     </div>`
+//   );
+//   instance.show();
+//   console.log(e.target);
+// };
